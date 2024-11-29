73c8aee029d392712e07463aa2b6d61f61e32ea8
dmitridrozdov <dm.drozdov@gmail.com>
2024-11-29 15:58:57 +1100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -42,9 +43,9 @@ export default function RootLayout({
         fontSans.variable
       )}
     >
-      {/* <ThemeProvider attribute="class" defaultTheme="dark"> */}
+      <ThemeProvider attribute="class" defaultTheme="dark">
         {children}
-      {/* </ThemeProvider> */}
+      </ThemeProvider>
     </body>
   </html>
   );
