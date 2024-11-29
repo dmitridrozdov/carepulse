4433139346eae0952328abbe2fce20b27ab765b4
dmitridrozdov <dm.drozdov@gmail.com>
2024-11-29 15:59:00 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Button>Test</Button>
     </div>
   );
 }
